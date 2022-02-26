345c34524e0ca1fa8c423f02fa13ab5488ec0522
waisec <waisec97@gmail.com>
2022-02-26 21:46:11 +0900

    BJ1260 revivision_2

--- BJ1260.py
+++ BJ1260.py
@@ -1,13 +1,15 @@
+import sys; input = sys.stdin.readline
 from collections import deque
 
 # DFS 함수 정의
-def DFS(start, dfs_check, graph):
+def DFS(start, vertex, dfs_check, graph):
+
     pos = start - 1
     print(start, end=" ")
     dfs_check[pos] = 1
-    for i in range(len(graph)):
-        if graph[pos][i] == 1 and dfs_check[i] == 0 :
-            DFS(i + 1, dfs_check, graph)    # 여기서 i+1을 해주지 않고 i를 해서 무한루프가 나온 것!!
+    for i in range(vertex):
+        if i in graph[pos] and dfs_check[i] == 0 :
+            DFS(i + 1, vertex, dfs_check, graph)    # 여기서 i+1을 해주지 않고 i를 해서 무한루프가 나온 것!!
 
 # BFS 함수 정의
 def BFS(start, vertex, graph):
@@ -23,45 +25,37 @@ def BFS(start, vertex, graph):
         for i in range(0, vertex):
             if i == pos:
                 continue
-            if check[i] == 0 and graph[pos][i] == 1:
+            if check[i] == 0 and i in graph[pos]:
                 q.append(i)
                 check[i] = 1
                 
         print(pos + 1, end=" ")  # queue에서 나오는 순서대로 탐색이기 때문에 출력
 
 # 입력받은 값으로 그래프 그리는 함수
-def Graph(vertex, edge_list):
-    
-    # 전체가 vertex * vertex 크기고 0으로 채워진 graph
-    graph = [[0 for col in range(vertex)] for row in range(vertex)]
-    
-    for i in range(len(edge_list)):
-        v1, v2 = edge_list[i]
-        graph[v1-1][v2-1] = 1
-        graph[v2-1][v1-1] = 1
+def Graph(edge):    
+    # 전체가 vertex * vertex 크기고 0으로 채워진 graph 
+    adj_list = [[] for _ in  range(edge)]
 
-    return graph
+    for _ in range(edge):
+        v1, v2 = map(int, input().split())
+        adj_list[v1-1].append(v2-1)
+        adj_list[v2-1].append(v1-1)
+    
+    return adj_list
 
 # 값 입력
 vertex, edge, start = map(int, input().split())
 # print(vertex, edge, start) input verify
 
-edge_list = []
 check = [0] * vertex
 
-for _ in range(edge):
-    v1, v2 = map(int, input().split())
-    edge_list.append((v1, v2))
-
-# print(edge_list)
-
 # graph 생성
-graph = Graph(vertex, edge_list)
+graph = Graph(edge)
 # print(graph)
 
 #DFS 실행
 dfs_check = [0] * vertex
-DFS(start, dfs_check, graph)
+DFS(start, vertex, dfs_check, graph)
 
 #newline
 print()
